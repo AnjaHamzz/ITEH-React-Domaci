d121f4b59eff0226b9346bbc1c0f8a79de93d97b
AnjaHamzz <anjahmzt1998@gmail.com>
2022-06-18 23:18:38 +0200

    Primena useStatea u Konvertor komponenti

--- src/components/Konvertor.js
+++ src/components/Konvertor.js
@@ -1,9 +1,24 @@
-
+import {useState} from 'react'
 
 function Konvertor(){
 
+    const [valuta1, setValuta1] = useState();
+    const [valuta2, setValuta2] = useState();
+    const [iznos, setIznos] = useState();
 
+    function updateValuta1(e){
+        setValuta1(e.target.value);
+    }
 
+    function updateValuta2(e){
+        setValuta2(e.target.value);
+    }
+
+    function updateIznos(e){
+        setIznos(e.target.value);
+    }
+
+    console.log(valuta1, valuta2, iznos)
 
     return (
         <div>
@@ -12,11 +27,11 @@ function Konvertor(){
 
             <div id="konvertor-div">
 
-                <input type="text"  className="form-control text-center knv-polja" placeholder="Valuta 1..." />
+                <input type="text" onChange={updateValuta1}  className="form-control text-center knv-polja" placeholder="Valuta 1..." />
 
-                <input type="text"  className="form-control text-center knv-polja" placeholder="Valuta 2..."/>
+                <input type="text" onChange={updateValuta2} className="form-control text-center knv-polja" placeholder="Valuta 2..."/>
 
-                <input type="text"  className="form-control text-center knv-polja" placeholder="Iznos..."/>
+                <input type="text" onChange={updateIznos}  className="form-control text-center knv-polja" placeholder="Iznos..."/>
 
 
                 <button className="btn btn-danger mt-3">Konvert</button>
