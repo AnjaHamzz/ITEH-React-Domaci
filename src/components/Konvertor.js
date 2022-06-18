fec7930110124ab62d750ffa462bd220b8ce7b1b
AnjaHamzz <anjahmzt1998@gmail.com>
2022-06-18 23:12:16 +0200

    Struktura Konvertor komponente

--- src/components/Konvertor.js
+++ src/components/Konvertor.js
@@ -1,8 +1,29 @@
 
 
 function Konvertor(){
+
+
+
+
     return (
         <div>
+
+            <h2 id="konv-head">Konvertor valuta</h2>
+
+            <div id="konvertor-div">
+
+                <input type="text"  className="form-control text-center knv-polja" placeholder="Valuta 1..." />
+
+                <input type="text"  className="form-control text-center knv-polja" placeholder="Valuta 2..."/>
+
+                <input type="text"  className="form-control text-center knv-polja" placeholder="Iznos..."/>
+
+
+                <button className="btn btn-danger mt-3">Konvert</button>
+ 
+            </div>
+
+
         </div>
     )
 }
